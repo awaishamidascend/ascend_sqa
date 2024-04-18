d925da1e2fc86ece0d855bcc9630cf77e106620f
LENOVO <LENOVO@Awais-Office-Laptop>
2024-04-18 17:56:03 +0500

    Aman code update

--- automate/src/main/java/test/automate/Efx_Aman.java
+++ automate/src/main/java/test/automate/Efx_Aman.java
@@ -8,32 +8,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Efx_Aman {
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         
+        // Maximize the browser window
+        driver.manage().window().maximize();
+        
         driver.get("https://efficax-obligation.ascend.com.sa/login?isQA=true");
+        
         Thread.sleep(10000);
-        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("system@ascend.com");
-        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("eod777");
+        
+        // Find and fill in the email and password fields
+        driver.findElement(By.xpath("//input[@placeholder='Email']"))
+              .sendKeys("system@ascend.com");
+        driver.findElement(By.xpath("//input[@placeholder='Password']"))
+              .sendKeys("eod777");
+        
+        // Click on the login button
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(50000);
+        
+        Thread.sleep(5000);
+        
         String otp = "786786";
-        // Find the OTP input field
-        //WebElement otpInput = driver.findElement(By.xpath("//input[@aria-label='Please enter OTP character 1']"));
-        // Loop through each digit of the OTP and enter it into the input field
+        
+        // Find the OTP input field and enter OTP
         for (int i = 0; i < otp.length(); i++) {
             char digit = otp.charAt(i);
             String digitAsString = Character.toString(digit);
-            // Construct XPath for locating each box of the OTP input field
             String xpath = "//input[@aria-label='Please enter OTP character " + (i + 1) + "']";
             WebElement otpBox = driver.findElement(By.xpath(xpath));
             otpBox.sendKeys(digitAsString);
         }
-
+        
+        // Click on the OTP login button
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
+        
+        Thread.sleep(5000);
+        
     }
 }
