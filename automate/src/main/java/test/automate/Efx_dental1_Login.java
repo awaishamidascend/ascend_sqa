cc978651131198ec4f79fdaa95422dfb074ce281
LENOVO <LENOVO@Awais-Office-Laptop>
2024-04-21 12:16:42 +0500

    Added libraries in Dental

--- automate/src/main/java/test/automate/Efx_dental1_Login.java
+++ automate/src/main/java/test/automate/Efx_dental1_Login.java
@@ -3,7 +3,8 @@ package test.automate;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Efx_dental1_Login {
 
