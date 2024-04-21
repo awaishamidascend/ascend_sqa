4457e1094ab699e514b7b14ccea3fb377dddb352
LENOVO <LENOVO@Awais-Office-Laptop>
2024-04-21 12:59:54 +0500

    Enhanced Code

--- automate/src/main/java/test/automate/Efx_scoc.java
+++ automate/src/main/java/test/automate/Efx_scoc.java
@@ -14,35 +14,49 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Efx_scoc {
 
     public static void main(String[] args) {
-    	
+        // Setup WebDriverManager to manage the WebDriver binaries
         WebDriverManager.chromedriver().setup();
+        
+        // Create a new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
-        driver.manage().window().maximize(); // Maximizing the browser window
+        // Maximizing the browser window
+        driver.manage().window().maximize();
 
+        // Setting timeouts for WebDriver
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
+        // Navigating to the login page
         driver.get("https://efficax-ssr.kakashi.app/login");
 
+        // Creating WebDriverWait instance with timeout
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        // Waiting for email input field to be clickable
         WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Email']")));
 
+        // Entering email
         emailInput.sendKeys("awais.hamid@ascend.com.sa");
 
+        // Creating another WebDriverWait instance with timeout
         WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        // Waiting for password input field to be clickable
         WebElement passwordInput = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Password']")));
 
+        // Entering password
         passwordInput.sendKeys("eod777");
 
+        // Clicking on the submit button
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+        // Finding and clicking on an element using CSS selector
         WebElement indicatorElement = driver.findElement(By.cssSelector(".select__indicator.select__dropdown-indicator.css-tlfecz-indicatorContainer"));
-
         indicatorElement.click();
         
+        // Closing the browser
+        driver.quit();
     }
 }
