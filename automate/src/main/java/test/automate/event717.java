65fe20b2b2b40283efe30dd8fc301e13db782c16
Ascend <Ascend@DESKTOP-IC83GL6>
2024-04-24 11:22:10 +0500

    Classes moved

---
+++ automate/src/main/java/test/automate/event717.java
@@ -0,0 +1,45 @@
+package test.automate;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class event717 {
+	public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get("https://efficax-717.kakashi.app/login");
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("dev-romeesa.ashfaq@ascend.com.sa");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("eod777");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(1000);
+        String otp = "786786";
+        // Find the OTP input field
+        //WebElement otpInput = driver.findElement(By.xpath("//input[@aria-label='Please enter OTP character 1']"));
+        // Loop through each digit of the OTP and enter it into the input field
+        for (int i = 0; i < otp.length(); i++) {
+            char digit = otp.charAt(i);
+            String digitAsString = Character.toString(digit);
+            // Construct XPath for locating each box of the OTP input field
+            String xpath = "//input[@aria-label='Please enter OTP character " + (i + 1) + "']";
+            WebElement otpBox = driver.findElement(By.xpath(xpath));
+            otpBox.sendKeys(digitAsString);
+        }
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'btn btn-primary my-1 btn btn-outline-secondary')]")).click();
+        Thread.sleep(1000);
+        
+	
+	
+	//Test
+	
+	
+	
+	
+	}}
