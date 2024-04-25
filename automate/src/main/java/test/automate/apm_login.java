c74828540133b9871e8c60b6167d3bfa4748884c
Awais Hamid <awais.hamid@ascend.com.sa>
2024-04-25 12:59:44 +0500

    APM Login

--- automate/src/main/java/test/automate/apm_login.java
+++ automate/src/main/java/test/automate/apm_login.java
@@ -1,10 +1,22 @@
 package test.automate;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class apm_login {
 
 	public static void main(String[] args) {
 		
-		
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://apm.ascend.com.sa/");
 		
 		
 	}
