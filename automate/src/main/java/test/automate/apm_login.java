ba1f0d0c6fa22d771918cc7c1160e060df381da1
Awais Hamid <awais.hamid@ascend.com.sa>
2024-05-20 14:59:08 +0500

    APM Login Success - Awais

--- automate/src/main/java/test/automate/apm_login.java
+++ automate/src/main/java/test/automate/apm_login.java
@@ -1,6 +1,10 @@
 package test.automate;
 
 import java.time.Duration;
+
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class apm_login {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -17,6 +21,23 @@ public class apm_login {
 
         driver.get("https://apm.ascend.com.sa/");
 		
+        driver.findElement(By.cssSelector("input[placeholder='Email / Phone number']")).sendKeys("awais.hamid@ascend.com.sa");
+		
+        driver.findElement(By.cssSelector("input[placeholder='Password']")).sendKeys("Ascend@745382");
+		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		Thread.sleep(5000);
+		
+		driver.findElement(By.xpath("(//span[@class='menu-item menu-title text-capitalize'][normalize-space()='All'])[1]")).click();
+		
+		Thread.sleep(5000);
+
+		driver.findElement(By.cssSelector("#Export")).click();
+		
+		driver.findElement(By.className("spinner-border text-white")).click();
+
+
 		
 	}
 
