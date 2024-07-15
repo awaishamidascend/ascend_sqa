fa647124480b03eecb90eb2a0728858ca3ec7f78
Awais Hamid <awais.hamid@ascend.com.sa>
2024-07-15 16:55:29 +0500

    CC

--- automate/src/main/java/test/automate/CreateTicket.java
+++ automate/src/main/java/test/automate/CreateTicket.java
@@ -9,57 +9,62 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CreateTicket {
 
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		WebDriverManager.chromedriver().setup();
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         
-        driver.get("https://nhcc.ascend.com.sa");
-        
+        System.out.println("Opening the application...");
+        driver.get("https://nhcc.kakashi.app/");
         driver.manage().window().maximize();
+        System.out.println("Application opened.");
+        
+        System.out.println("Entering email...");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("user@test.com");
+        System.out.println("Email entered.");
 
-     
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("khadija.yasin@ascend.com.sa");
-        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Ascend@2040");
+        System.out.println("Entering password...");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Khadija@123");
+        System.out.println("Password entered.");
+
+        System.out.println("Clicking login button...");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         Thread.sleep(5000);
-		
-		
-        //click on Tickets
-      driver.findElement(By.xpath("//span[@class='menu-title text-truncate']//span[contains(text(),'Tickets')]")).click();
-      
-      //create ticket
-      driver.findElement(By.xpath("//span[contains(text(),'Create Ticket')]")).click();
-      Thread.sleep(5000);
+        System.out.println("Logged in successfully.");
 
-      //add subject
+        System.out.println("Navigating to Tickets...");
+        driver.findElement(By.xpath("//span[@class='menu-title text-truncate']//span[contains(text(),'Tickets')]")).click();
+        System.out.println("Clicked on Tickets.");
+
+        System.out.println("Navigating to Create Ticket...");
+        driver.findElement(By.xpath("//span[contains(text(),'Create Ticket')]")).click();
+        Thread.sleep(5000);
+        System.out.println("Clicked on Create Ticket.");
+
+        System.out.println("Entering subject...");
         driver.findElement(By.xpath("//input[@placeholder='Subject']")).sendKeys("test subject");
+        System.out.println("Subject entered.");
 
-      
-      //add description
+        System.out.println("Entering description...");
         driver.findElement(By.xpath("//div[@class='fr-element fr-view fr-element-scroll-visible']//p")).sendKeys("test description");
-      
-        //Next page
+        System.out.println("Description entered.");
+
+        System.out.println("Proceeding to the next page...");
         driver.findElement(By.xpath("//div[@id='General-Information']//button[contains(@type,'submit')][normalize-space()='Next']")).click();
-     
-        // select team
-     
+        System.out.println("Proceeded to the next page.");
+
+        System.out.println("Selecting team...");
         driver.findElement(By.xpath("//div[@id='Additional-Information']//div[contains(@class,'row')]//div[1]//div[1]//label[1]")).click();
+        System.out.println("Team selected.");
 
+        System.out.println("Selecting option in dropdown...");
         String option = "t";
-
         WebElement dropdown = driver.findElement(By.xpath("//select/option[contains(text(), '" + option + "')]"));
-
         dropdown.click();
+        System.out.println("Dropdown option selected.");
 
-        System.out.println("clicked");
-        
-       System.out.println("Test completed. Closing the browser...");
-
-     // Close the browser
+        System.out.println("Test completed. Closing the browser...");
         driver.quit();
-	}
-
-	}
-
-
+        System.out.println("Browser closed.");
+    }
+}
+s
