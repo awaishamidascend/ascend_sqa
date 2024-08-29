1c0f6fc8ad6aae2f56b271401168529fb40dd7c6
RidAaaaaaaa <128960770+RidAaaaaaaa@users.noreply.github.com>
2024-08-29 16:56:56 +0500

    717 export

--- automate/src/main/java/test/automate/Export717.java
+++ automate/src/main/java/test/automate/Export717.java
@@ -4,18 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Export717 {
 	public static void main(String[] args) throws InterruptedException {
-
-		WebDriverManager.chromedriver().setup();
+	    WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         
-        driver.get("https://moh717.ascend.com.sa/login");
+        driver.get("https://efficax-717.kakashi.app/login");
         Thread.sleep(10000);
-        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("system@efficax.com");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rida.khan@ascend.com.sa");
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("eod777");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         Thread.sleep(10000);
@@ -32,7 +32,27 @@ public class Export717 {
             otpBox.sendKeys(digitAsString);
         }
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(10000);  
-        //export
-        driver.findElement(By.xpath("//button[@class='export-label']//*[name()='svg']")).click();
-	} }
+        Thread.sleep(10000);
+        
+        //role selection
+        driver.findElement(By.xpath("//div[@class='select__indicator select__dropdown-indicator css-qj08tm-indicatorContainer']")).click();
+        System.out.println("dropdown click");
+
+        //select admin role
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/div/form/div/div[3]/div/div/div[2]/div/div[2]")).click();
+        System.out.println("role select");
+
+        Thread.sleep(1000);
+        
+
+        //submit button
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/div/div/div/div[2]/div[2]/div/div/form/div/button")).click();
+        System.out.println("submit");
+
+        Thread.sleep(1000);
+        //export button
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/div/div[2]/button[2]")).click();
+        System.out.println("export");
+
+	}
+}
