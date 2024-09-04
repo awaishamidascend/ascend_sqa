96f61cf9827b90bcd27c57d519f67d815f71e4d3
Awais Hamid <111340520+awaishamidascend@users.noreply.github.com>
2024-09-04 13:13:42 +0500

    Fixed

--- automate/src/main/java/test/automate/OperationCreate.java
+++ automate/src/main/java/test/automate/OperationCreate.java
@@ -1,7 +1,6 @@
 package test.automate;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -9,79 +8,63 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OperationCreate {
 
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("https://nhcc.kakashi.app");
+        driver.manage().window().maximize();
 
-		driver.get("https://nhcc.kakashi.app");
-		driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("khadija.yasin@ascend.com.sa");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Alpha@2040");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(5000);
 
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("khadija.yasin@ascend.com.sa");
-		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Alpha@2040");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		Thread.sleep(10000);
+        System.out.println("Opening Organization Selection...");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement orgSelectContainer = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select__input-container css-19bb58m']")));
+        orgSelectContainer.click();
+        System.out.println("Organization Selection Opened successfully.");
 
-		System.out.println("Opening Organization Selection...");
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement orgSelectContainer = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select__input-container css-19bb58m']")));
-		orgSelectContainer.click();
-		System.out.println("Organization Selection Opened successfully.");
+        Thread.sleep(2000);
 
-		// Wait for the dropdown to load
-		Thread.sleep(2000);
+        WebElement optionElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select__option css-10wo9uf-option' and text()='Technical Support']")));
+        optionElement.click();
+        System.out.println("Clicked on the 'Technical Support' option");
 
-		// Locate the element by its XPath and click it
-		WebElement optionElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='select__option css-10wo9uf-option' and text()='Technical Support']")));
-		optionElement.click();
-		System.out.println("Clicked on the 'Technical Support' option");
+        WebElement continueButton = driver.findElement(By.xpath("//button[contains(@class, 'CustomButton') and text()='Continue']"));
+        continueButton.click();
 
-		// Locate the element using XPath and click it
-		WebElement continueButton = driver.findElement(By.xpath("//button[contains(@class, 'CustomButton') and text()='Continue']"));
-		continueButton.click();
+        Thread.sleep(5000);
 
-		Thread.sleep(7000);
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//span[contains(text(),'Operation Centers')]")).click();
+        Thread.sleep(5000);
+        System.out.println("Clicked on the operation center");
 
-		//click on Operation center
-		Thread.sleep(10000);
-		driver.findElement(By.xpath("//span[contains(text(),'Operation Centers')]")).click();
-		Thread.sleep(7000);
-		System.out.println("Clicked on the operation cenbter");
+        driver.findElement(By.xpath("(//span[contains(text(),'Create Operation Center')])[1]")).click();
+        System.out.println("Clicked on the create operation");
 
-		//create operation center click
-		driver.findElement(By.xpath("(//span[contains(text(),'Create Operation Center')])[1]")).click();
-		System.out.println("Clicked on the create operation");
+        Thread.sleep(5000);
 
-		Thread.sleep(5000);
+        driver.findElement(By.xpath("//input[@id='orgName']")).sendKeys("New");
+        Thread.sleep(5000);
 
-		//name operation center
-		driver.findElement(By.xpath("//input[@id='orgName']")).sendKeys("New");
-		Thread.sleep(5000);
+        System.out.println("Opening SHOW FIELDS");
 
-		//select show fields
+        WebElement dropdownContainer = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]"));
+        dropdownContainer.click();
+        System.out.println("SHOW FIELDS dropdown opened successfully.");
 
-		System.out.println("Opening SHOW FIELDS");
+        Thread.sleep(2000);
+        
+        WebElement drop = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'select__option css-10wo9uf-option') and text()='Teams']")));
+        drop.click();
+        System.out.println("Clicked on the Dropdown Option 'Teams'");
 
-		WebElement dropdownContainer = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]"));
-
-		// Click on the dropdown container to expand it
-		dropdownContainer.click();
-		//((JavascriptExecutor) driver).executeScript("arguments[0].click();", orgSelectContainer1);
-
-		System.out.println("SHOW FIELDS dropdown opened successfully.");
-
-		// Wait for the dropdown to load
-		Thread.sleep(2000);
-
-		// Locate the element by its XPath and click it
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]")).click();
-		System.out.println("Clicked on the Dropdown Options TEAMS");
-	}
-
+    }
 }
